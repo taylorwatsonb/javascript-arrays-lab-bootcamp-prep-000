a0909e05d830a8f111f8eba5ed2572f5b89c788d
Taylor Watson <taylorwatsonb@gmail.com>
2018-09-26 00:00:27 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -11,8 +11,8 @@ describe('Arrays', function() {
 
   describe('destructivelyAppendKitten(name)', function() {
     it('appends a kitten to the end of the kittens array', function() {
-      destructivelyAppendKitten('Ralph')
-      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield", "Ralph"])
+      const test = destructivelyAppendKitten('Ralph')
+      expect(test).toEqual(["Milo", "Otis", "Garfield", "Ralph"])
     })
   })
 
